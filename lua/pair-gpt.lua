ccffaa61b17a36ea953d781a713d59c925c85605
Miguel Palhas <mpalhas@gmail.com>
2022-12-05 20:23:33 +0000

    code review

--- lua/pair-gpt.lua
+++ lua/pair-gpt.lua
@@ -9,6 +9,7 @@ local function clean_prompt(prompt)
 
   ret = prompt:gsub("^[" .. stripable .. "]", "")
   ret = ret:gsub("[" .. stripable .. "]$", "")
+  ret = ret:gsub("\"", "\\\"")
 
   return ret
 end
@@ -31,34 +32,55 @@ local function pair_cmd(subcmd, lang, prompt)
   return lines
 end
 
+local function get_visual_selection(buf)
+  local s_start = vim.fn.getpos("'<")
+  local s_end = vim.fn.getpos("'>")
+  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+  local lines = vim.api.nvim_buf_get_lines(buf, s_start[2] - 1, s_end[2], false)
+  lines[1] = string.sub(lines[1], s_start[3], -1)
+  if n_lines == 1 then
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+  else
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+  end
+  return table.concat(lines, '\n')
+end
+
 local function write()
   local win = api.nvim_get_current_win()
-
   local line = api.nvim_get_current_line()
   local lang = o.syntax
   local buf = api.nvim_get_current_buf()
   local linenr = api.nvim_win_get_cursor(win)[1]
+
+  -- clean prompt. remove comment characters
   local prompt = clean_prompt(line)
 
+  -- query OpenAI. this is blocking
   local output = pair_cmd("write", lang, prompt)
 
+  -- write output right below the prompt line
   api.nvim_buf_set_lines(buf, linenr, linenr, false, output)
 end
 
--- M.send_selection = function()
---     local line1 = vim.fn.getpos("'<")[2]
---     local line2 = vim.fn.getpos("'>")[2]
---
---     local lines = ""
---     for _, v in pairs(vim.fn.getline(line1, line2)) do
---         lines = lines .. v .. "\n"
---     end
---
---     M.send(lines)
--- end
+local function refactor()
+  -- local win = api.nvim_get_current_win()
+  -- local line = api.nvim_get_current_line()
+  local lang = o.syntax
+  local buf = api.nvim_get_current_buf()
+  -- local linenr = api.nvim_win_get_cursor(win)[1]
+
+  local input = get_visual_selection(buf)
+  local output = pair_cmd("refactor", lang, input)
+
+  api.nvim_buf_set_lines(buf, 0, 0, false, output)
+
+end
+
 --
 -- function _G.reload_current_file() vim.cmd(":luafile %") end
 
 return {
-  write = write
+  write = write,
+  refactor = refactor
 }
--- plugin/pair-gpt.vim
+++ plugin/pair-gpt.vim
@@ -3,8 +3,8 @@ if exists('g:loaded_pair_gpt') | finish | endif
 let s:save_cpo = &cpo
 set cpo&vim
 
-command! PairGPTWrite lua require'pair-gpt'.write()
-command! PairGPTRefactor lua require'pair-gpt'.refactor()
+command! -range PairGPTWrite lua require'pair-gpt'.write()
+command! -range PairGPTRefactor lua require'pair-gpt'.refactor()
 
 let &cpo = s:save_cpo
 unlet s:save_cpo
