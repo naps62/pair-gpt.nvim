8b0de1b8ba15bee8776093c43b29d1ef82f5c14f
Miguel Palhas <mpalhas@gmail.com>
2022-12-05 21:04:47 +0000

    Refactor feature ready

--- README.md
+++ README.md
@@ -7,19 +7,27 @@
 
 A GPT-assisted pair programming partner for Neovim.
 
+## Features
+
+**Write code from text prompts**
+
 [![asciicast](https://asciinema.org/a/gIXsFqG3ZDVhVFRkpIkBipz9I.svg)](https://asciinema.org/a/gIXsFqG3ZDVhVFRkpIkBipz9I)
 
-# Prerequisites
+**Refactor existing code**
+
+[![asciicast](https://asciinema.org/a/0IrC8uV5aHt4kXnfJK273Hcez.svg)](https://asciinema.org/a/0IrC8uV5aHt4kXnfJK273Hcez)
+
+## Prerequisites
 
 You need an account for [OpenAI's API][openai-api]. Check out the [pricing page][openai-pricing] to see how far you can go with the free plan (spoiler alert: should be more than enough to take this plugin out for a testdrive).
 
-# Install
+## Install
 
 1. Create an API key through [OpenAI's dashboard][openai-apikeys], and set it to an `OPENAI_API_KEY` environment variable.
 2. Install the `pair-gpt` binary: `cargo install --git https://github.com/naps62/pair-gpt`
 3. Setup the vim plugin (see below)
 
-## Vim plugin setup
+### Vim plugin setup
 
 With [packer][packer]:
 
@@ -27,7 +35,7 @@ With [packer][packer]:
 use 'naps62/pair-gpt.nvim'
 ```
 
-# Usage
+## Usage
 
 Open a file with your editor. ensure filetype detection is live (this plugin uses that to pass down to the bot).
 
--- lua/pair-gpt.lua
+++ lua/pair-gpt.lua
@@ -1,5 +1,6 @@
 local api = vim.api
 local o = vim.o
+local fn = vim.fn
 
 local bin = "pair-gpt"
 
@@ -16,7 +17,6 @@ end
 
 local function pair_cmd(subcmd, lang, prompt)
   local cmd = bin .. " --lang " .. lang .. " " .. subcmd .. " \" " .. prompt .. "\""
-  print(cmd)
 
   -- run cmd
   local handle = assert(io.popen(cmd, 'r'))
@@ -37,13 +37,14 @@ local function get_visual_selection(buf)
   local s_end = vim.fn.getpos("'>")
   local n_lines = math.abs(s_end[2] - s_start[2]) + 1
   local lines = vim.api.nvim_buf_get_lines(buf, s_start[2] - 1, s_end[2], false)
-  lines[1] = string.sub(lines[1], s_start[3], -1)
-  if n_lines == 1 then
-    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
-  else
-    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
-  end
-  return table.concat(lines, '\n')
+  -- TODO currently grabbing entire lines, not exact visual selection
+  -- lines[1] = string.sub(lines[1], s_start[3], -1)
+  -- if n_lines == 1 then
+  --   lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+  -- else
+  --   lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+  -- end
+  return table.concat(lines, '\\n')
 end
 
 local function write()
@@ -59,21 +60,25 @@ local function write()
   -- query OpenAI. this is blocking
   local output = pair_cmd("write", lang, prompt)
 
-  -- write output right below the prompt line
-  api.nvim_buf_set_lines(buf, linenr, linenr, false, output)
+  api.nvim_buf_set_lines(buf, 0, 0, false, output)
 end
 
 local function refactor()
+  local s_start = fn.getpos("'<")
+  local s_end = fn.getpos("'>")
   -- local win = api.nvim_get_current_win()
   -- local line = api.nvim_get_current_line()
   local lang = o.syntax
   local buf = api.nvim_get_current_buf()
   -- local linenr = api.nvim_win_get_cursor(win)[1]
 
-  local input = get_visual_selection(buf)
+  local input = clean_prompt(get_visual_selection(buf))
   local output = pair_cmd("refactor", lang, input)
 
-  api.nvim_buf_set_lines(buf, 0, 0, false, output)
+  -- writ_ output right below the prompt line
+  -- TODO currently replacing whole lines, not exact visual selection
+  -- api.nvim_buf_set_text(buf, s_start[2] - 1, s_start[3] - 1, s_end[2] - 1, s_end[3], output)
+  api.nvim_buf_set_lines(buf, s_start[2] - 1, s_end[2], false, output)
 
 end
 
